87df158a3c90ac198fc04a07c8d1b9049aaa5de5
swathi M <sumanthnathreddy.buchupalli@ltts.com>
2020-10-07 22:28:35 +0530

    first

---
+++ inc/function.h
@@ -0,0 +1,17 @@
+#ifndef __FUNCTION_H__
+#define __FUNCTION_H__
+
+#include <stdio.h>
+
+#include<math.h>
+#define KEY "Enter the calculator Operation you want to do:"
+void addition();
+void subtraction();
+void multiplication();
+void division();
+void modulus();
+void power();
+int factorial();
+void calculator_operations();
+
+#endif
